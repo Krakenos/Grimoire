5457a3b81c071f1459bbb595eea64b2b9a1a9252
Krakenos <1944193+Krakenos@users.noreply.github.com>
2023-11-26 21:01:03 +0100

    added context filling

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import json
-import os
 import re
-import sqlite3
 
 import requests
 import spacy
@@ -16,11 +14,13 @@ from request_schemas import KAIGenerationInputSchema, KAITokenCountSchema
 app = FastAPI()
 nlp = spacy.load("en_core_web_trf")
 KOBOLD_URL = 'http://127.0.0.1:5001'
+SIDE_API_URL = 'http://127.0.0.1:5002'
+CONTEXT_PERCENTAGE = 0.25
 db = create_engine('sqlite:///db.sqlite3')
 
 
 def make_summary_prompt(session, term):
-    prompt = f'<|system|>Based on following text summarize what or who {term} is. Keep explanation short\n'
+    prompt = f'<|system|>Based on following text summarize what or who {term} is. Keep explanation short<|user|>'
     instance = session.query(Knowledge).filter_by(entity=term).first()
     for message in instance.messages:
         prompt += message.message + '\n'
@@ -31,8 +31,9 @@ def make_summary_prompt(session, term):
 def summarize(session, term):
     prompt = make_summary_prompt(session, term)
     json = {'prompt': prompt, 'max_length': 350}
-    kobold_response = requests.post(KOBOLD_URL + '/api/v1/generate', json=json)
+    kobold_response = requests.post(SIDE_API_URL + '/api/v1/generate', json=json)
     print(kobold_response)
+
 
 def orm_get_or_create(session, model, **kwargs):
     instance = session.query(model).filter_by(**kwargs).first()
@@ -48,7 +49,7 @@ def orm_get_or_create(session, model, **kwargs):
 def process_prompt(prompt):
     print(prompt)
     banned_labels = ['DATE', 'CARDINAL', 'ORDINAL']
-    messages = re.split(r"<\|user\|>|<\|model\|>", prompt)
+    messages = re.split(r"<\|user\|>|<\|model\|>", prompt)  # TODO make the instruct mode in/out changeable
     last_messages = messages[-3:-1]
     docs = list(nlp.pipe(last_messages))
     with Session(db) as session:
@@ -66,6 +67,65 @@ def process_prompt(prompt):
             for entity in doc.ents:
                 print(entity.text, entity.label_, spacy.explain(entity.label_))
                 summarize(session, entity.text)
+
+
+def count_context(text):
+    token_count_endpoint = KOBOLD_URL + '/api/extra/tokencount'
+    request_body = {'prompt': text}
+    kobold_response = requests.post(token_count_endpoint, json=request_body)
+    value = int(kobold_response.json()['value'])
+    return value
+
+
+def get_context_length(api_url: str) -> int:
+    length_endpoint = api_url + '/v1/config/max_context_length'
+    kobold_response = requests.get(length_endpoint)
+    value = int(kobold_response.json()['value'])
+    return value
+
+
+def fill_context(prompt):
+    max_context = get_context_length(KOBOLD_URL)
+    max_memoir_context = max_context * CONTEXT_PERCENTAGE
+    banned_labels = ['DATE', 'CARDINAL', 'ORDINAL']
+    messages = re.split(r"<\|user\|>|<\|model\|>", prompt)  # TODO make the instruct mode in/out changeable
+    prompt_definitions = messages[0]  # first portion should always be instruction and char definitions
+    docs = list(nlp.pipe(messages))
+    full_ent_list = []
+    for doc in docs:
+        print(doc.text_with_ws)
+        ent_list = [(str(ent), ent.label_) for ent in doc.ents if ent.label_ not in banned_labels]
+        full_ent_list += ent_list
+    unique_ents = list(dict.fromkeys(full_ent_list[::-1]))  # unique ents ordered from bottom of context
+    summaries = []
+    with Session(db) as session:
+        for ent in unique_ents:
+            instances = session.query(Knowledge).filter(Knowledge.entity == ent[0],
+                                                        Knowledge.entity_type == 'NAMED ENTITY',
+                                                        Knowledge.entity_label == ent[1], Knowledge.summary.isnot(None),
+                                                        Knowledge.summary.isnot(''), Knowledge.token_count.isnot(None),
+                                                        Knowledge.token_count.isnot(0))
+            instance = instances.first()
+            summaries.append((instance.summary, instance.token_count))
+    memoir_token_sum = sum([summary_tuple[1] for summary_tuple in summaries])
+    while memoir_token_sum > max_memoir_context:
+        summaries.pop()
+        memoir_token_sum = sum([summary_tuple[1] for summary_tuple in summaries])
+    memoir_text = ''
+    for summary in summaries:
+        memoir_text = memoir_text + summary[0] + '\n'
+    memoir_text_len = count_context(memoir_text)
+    definitions_context_len = count_context(prompt_definitions)
+    max_chat_context = max_context - definitions_context_len - memoir_text_len
+    starting_message = 1
+    messages_text = '\n'.join(messages[starting_message:])
+    messages_len = count_context(messages_text)
+    while messages_len > max_chat_context:
+        starting_message += 1
+        messages_text = '\n'.join(messages[starting_message:])
+        messages_len = count_context(messages_text)
+    final_prompt = '\n'.join([prompt_definitions, memoir_text, messages_text])
+    return final_prompt
 
 
 def get_passthrough(endpoint: str) -> dict:
