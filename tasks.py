22ea04de1c4a7329b4ad81a5a44a272f9c5f39f7
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-01-15 21:31:41 +0100

    adjust summary prompt for mistral

--- tasks.py
+++ tasks.py
@@ -7,18 +7,32 @@ from settings import SIDE_API_URL
 celery_app = Celery('tasks', broker='amqp://guest@localhost//')
 
 
-def make_summary_prompt(session, term):
-    prompt = f'<|system|>Based on following text summarize what or who {term} is. Keep explanation short<|user|>'
-    instance = session.query(Knowledge).filter_by(entity=term).first()
+def make_summary_prompt(session, term, chat_id):
+    prompt = f'<s>[INST] Based on following text describe {term}.\n\n'
+    instance = session.query(Knowledge).filter_by(entity=term, chat_id=chat_id).scalar()
     for message in instance.messages:
         prompt += message.message + '\n'
-    prompt += '<|model|>'
+    prompt += '[/INST]'
     return prompt
 
 
 @celery_app.task
-def summarize(session, term):
-    prompt = make_summary_prompt(session, term)
+def summarize(session, term, chat_id):
+    prompt = make_summary_prompt(session, term, chat_id)
     json = {'prompt': prompt, 'max_length': 350}
+    params = {
+        "temperature": 0.3,
+        "max_context_length": 4096,
+        "max_length": 350,
+        "stop": [
+            "</s>"
+        ],
+        "top_p": 0.95,
+        "top_k": 50,
+        "rep_pen": 1.2,
+        "stop_sequence": [
+            "</s>"
+        ]
+    }
     kobold_response = requests.post(SIDE_API_URL + '/api/v1/generate', json=json)
     print(kobold_response)
