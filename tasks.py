c21e405bf9b38cbffa8c0227d39d03fae5c34e4b
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-01-20 15:14:27 +0100

    log and save summaries

--- tasks.py
+++ tasks.py
@@ -2,9 +2,9 @@ import requests
 from celery import Celery
 
 from llm_utils import count_context
+from loggers import summary_logger
 from models import Knowledge
 from settings import SIDE_API_URL
-from loggers import summary_logger
 
 celery_app = Celery('tasks', broker='amqp://guest@localhost//')
 
@@ -23,7 +23,6 @@ def make_summary_prompt(session, term, label, chat_id, max_context):
             prompt = new_prompt
     prompt += '[/INST]'
     return prompt
-
 
 
 @celery_app.task
@@ -47,4 +46,8 @@ def summarize(session, term, label, chat_id, context_len=4096, response_len=300)
     kobold_response = requests.post(SIDE_API_URL + '/api/v1/generate', json=json)
     response = kobold_response.json()
     summary_text = response['results'][0]['text']
-    summary_logger.debug(f'{term} ({label}): {summary_text}')
+    knowledge_entry = session.query(Knowledge).filter_by(entity=term, chat_id=chat_id, entity_label=label).scalar()
+    knowledge_entry.summary_text = summary_text
+    knowledge_entry.token_count = count_context(term, 'KoboldAI', SIDE_API_URL)
+    summary_logger.debug(f'({knowledge_entry.token_count} tokens){term} ({label}): {summary_text}\n{json}')
+    session.commit()
