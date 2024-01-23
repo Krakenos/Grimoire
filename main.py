c6f2fff3f9ca155c589f33b763850f387cdf000c
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-01-23 18:45:09 +0100

    move loading env

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import uvicorn
+from dotenv import load_dotenv
 
 from memoir.api import app
 
 if __name__ == '__main__':
+    load_dotenv()
     uvicorn.run(app, host="127.0.0.1", port=5555)
--- memoir/core/settings.py
+++ memoir/core/settings.py
@@ -1,9 +1,5 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 MAIN_API_URL = os.getenv('MAIN_API_URL')
 MAIN_API_AUTH = os.getenv('MAIN_API_AUTH')
 SIDE_API_URL = os.getenv('SIDE_API_URL')
