fa488243987ef9b32b8dee74a7eb294bc3ed27e3
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-02-02 14:01:20 +0100

    moving stuff to settings

--- memoir/core/settings.py
+++ memoir/core/settings.py
@@ -14,9 +14,25 @@ CONTEXT_PERCENTAGE = float(os.getenv('CONTEXT_PERCENTAGE'))
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
 DB_ENGINE = os.getenv('DB_ENGINE')
 
+DEBUG = bool(os.getenv('DEBUG', False))
+LOG_PROMPTS = bool(os.getenv('LOG_PROMPTS', False))
 
 MODEL_INPUT_SEQUENCE = '### Instruction:'
 MODEL_OUTPUT_SEQUENCE = '### Response:'
 
-DEBUG = bool(os.getenv('DEBUG', False))
-LOG_PROMPTS = bool(os.getenv('LOG_PROMPTS', False))
+SUMMARIZATION_PROMPT = 'Based on following text describe {term}.'
+SUMMARIZATION_INPUT_SEQ = '[INST]'
+SUMMARIZATION_OUTPUT_SEQ = '[/INST]'
+SUMMARIZATION_START_TOKEN = '<s>'
+SUMMARIZATION_PARAMS = {
+    "top_p": 0.95,
+    "top_k": 50,
+    "rep_pen": 1.2,
+    "temperature": 0.6,
+    "stop": [
+        "</s>"
+    ],
+    "stop_sequence": [
+        "</s>"
+    ]
+}
--- memoir/core/tasks.py
+++ memoir/core/tasks.py
@@ -5,15 +5,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from memoir.common.llm_helpers import count_context
-from memoir.common.loggers import summary_logger, general_logger
-from memoir.core.settings import SIDE_API_URL, DB_ENGINE, CELERY_BROKER_URL, SIDE_API_BACKEND
+from memoir.common.loggers import summary_logger
+from memoir.core.settings import SIDE_API_URL, DB_ENGINE, CELERY_BROKER_URL, SIDE_API_BACKEND, SUMMARIZATION_PARAMS, \
+    SUMMARIZATION_PROMPT, SUMMARIZATION_START_TOKEN, SUMMARIZATION_INPUT_SEQ, SUMMARIZATION_OUTPUT_SEQ
 from memoir.db.models import Knowledge
 
 celery_app = Celery('tasks', broker=CELERY_BROKER_URL)
 
 
 def make_summary_prompt(session: Session, term: str, label: str, chat_id: str, max_context: int) -> str:
-    prompt = f'<s>[INST] Based on following text describe {term}.\n\n'
+    main_prompt = SUMMARIZATION_PROMPT.format(term=term)
+    prompt = f'{SUMMARIZATION_START_TOKEN}{SUMMARIZATION_INPUT_SEQ} {main_prompt}\n\n'
     instance = session.query(Knowledge).filter_by(entity=term, chat_id=chat_id, entity_label=label).scalar()
     if instance.summary is not None:
         summary = instance.summary + '\n'
@@ -22,12 +24,12 @@ def make_summary_prompt(session: Session, term: str, label: str, chat_id: str, m
     for message in instance.messages[::-1]:  # reverse order to start from latest message
         new_prompt = prompt + message.message + '\n'
         new_prompt += summary
-        new_tokens = count_context(new_prompt + '[/INST]', 'KoboldAI', SIDE_API_URL)
+        new_tokens = count_context(new_prompt + SUMMARIZATION_OUTPUT_SEQ, 'KoboldAI', SIDE_API_URL)
         if new_tokens >= max_context:
             break
         else:
             prompt = new_prompt
-    prompt += '[/INST]'
+    prompt += SUMMARIZATION_OUTPUT_SEQ
     return prompt
 
 
@@ -44,18 +46,9 @@ def summarize(term: str, label: str, chat_id: str, context_len: int = 4096,
         json = {
             'prompt': prompt,
             'max_length': response_len,
-            "temperature": 0.6,
             "max_context_length": context_len,
-            "stop": [
-                "</s>"
-            ],
-            "top_p": 0.95,
-            "top_k": 50,
-            "rep_pen": 1.2,
-            "stop_sequence": [
-                "</s>"
-            ]
         }
+        json.update(SUMMARIZATION_PARAMS)
         kobold_response = requests.post(SIDE_API_URL + '/api/v1/generate', json=json)
         response = kobold_response.json()
         summary_text = response['results'][0]['text']
