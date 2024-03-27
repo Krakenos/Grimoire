71823801a6d1311eff4d9105248022c2d2259caa
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-03-27 15:55:04 +0100

    change status code

--- grimoire/api/client.py
+++ grimoire/api/client.py
@@ -16,5 +16,5 @@ app.include_router(oai_generic.router)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
     general_logger.debug(f"{request}: {exc_str}")
-    content = {'status_code': 10422, 'message': exc_str, 'data': None}
+    content = {'status_code': 422, 'message': exc_str, 'data': None}
     return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
