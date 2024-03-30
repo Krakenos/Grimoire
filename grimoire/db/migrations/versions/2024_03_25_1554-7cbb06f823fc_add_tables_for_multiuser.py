d69180f388b20457e6d374ffde0af021ed0cc403
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-03-30 16:45:53 +0100

    more fixes in migration

--- grimoire/db/migrations/versions/2024_03_25_1554-7cbb06f823fc_add_tables_for_multiuser.py
+++ grimoire/db/migrations/versions/2024_03_25_1554-7cbb06f823fc_add_tables_for_multiuser.py
@@ -36,69 +36,63 @@ class User(Base):
     external_id = sa.Column(sa.String, nullable=False)
 
 
-class Message(Base):
-    __tablename__ = 'message'
-
-    id = sa.Column(sa.Integer, primary_key=True)
-    chat_id = sa.Column(sa.String, nullable=False)
-
 
 class KnowledgeOld(Base):
     __tablename__ = 'knowledge'
 
-    id = sa.Column(sa.Integer(), nullable=False),
-    chat_id = sa.Column(sa.String(), nullable=False)
-    entity = sa.Column(sa.String(), nullable=False)
-    entity_type = sa.Column(sa.String(), nullable=True)
-    entity_label = sa.Column(sa.String(), nullable=True)
-    summary = sa.Column(sa.String(), nullable=True)
-    token_count = sa.Column(sa.Integer(), nullable=True)
-    updated_date = sa.Column(sa.DateTime(), nullable=False)
-    update_at = sa.Column(sa.Integer(), nullable=True)
-    update_count = sa.Column(sa.Integer(), nullable=True)
+    id = sa.Column(sa.Integer, primary_key=True)
+    chat_id = sa.Column(sa.String, nullable=False)
+    entity = sa.Column(sa.String, nullable=False)
+    entity_type = sa.Column(sa.String, nullable=True)
+    entity_label = sa.Column(sa.String, nullable=True)
+    summary = sa.Column(sa.String, nullable=True)
+    token_count = sa.Column(sa.Integer, nullable=True)
+    updated_date = sa.Column(sa.DateTime, nullable=False)
+    update_at = sa.Column(sa.Integer, nullable=True)
+    update_count = sa.Column(sa.Integer, nullable=True)
 
 
 class KnowledgeNew(Base):
     __tablename__ = 'knowledge_new'
 
-    id = sa.Column(sa.Integer(), nullable=False),
-    chat_id = sa.Column(sa.Integer(), nullable=False)
-    entity = sa.Column(sa.String(), nullable=False)
-    entity_type = sa.Column(sa.String(), nullable=True)
-    entity_label = sa.Column(sa.String(), nullable=True)
-    summary = sa.Column(sa.String(), nullable=True)
-    token_count = sa.Column(sa.Integer(), nullable=True)
-    updated_date = sa.Column(sa.DateTime(), nullable=False)
-    update_at = sa.Column(sa.Integer(), nullable=True)
-    update_count = sa.Column(sa.Integer(), nullable=True)
+    id = sa.Column(sa.Integer, primary_key=True)
+    chat_id = sa.Column(sa.Integer, nullable=False)
+    entity = sa.Column(sa.String, nullable=False)
+    entity_type = sa.Column(sa.String, nullable=True)
+    entity_label = sa.Column(sa.String, nullable=True)
+    summary = sa.Column(sa.String, nullable=True)
+    token_count = sa.Column(sa.Integer, nullable=True)
+    updated_date = sa.Column(sa.DateTime, nullable=False)
+    update_at = sa.Column(sa.Integer, nullable=True)
+    update_count = sa.Column(sa.Integer, nullable=True)
 
 
 class MessageOld(Base):
     __tablename__ = 'message'
 
-    id = sa.Column(sa.Integer(), nullable=False)
-    chat_id = sa.Column(sa.String(), nullable=False)
-    message_index = sa.Column(sa.Integer(), nullable=False)
-    summary = sa.Column(sa.String(), nullable=True)
-    message = sa.Column(sa.String(), nullable=False)
-    summary_tokens = sa.Column(sa.Integer(), nullable=True)
-    message_tokens = sa.Column(sa.Integer(), nullable=True)
-    created_date = sa.Column(sa.DateTime(), nullable=False)
-    spacy_doc = sa.Column(sa.LargeBinary(), nullable=True)
+    id = sa.Column(sa.Integer, primary_key=True)
+    chat_id = sa.Column(sa.String, nullable=False)
+    message_index = sa.Column(sa.Integer, nullable=False)
+    summary = sa.Column(sa.String, nullable=True)
+    message = sa.Column(sa.String, nullable=False)
+    summary_tokens = sa.Column(sa.Integer, nullable=True)
+    message_tokens = sa.Column(sa.Integer, nullable=True)
+    created_date = sa.Column(sa.DateTime, nullable=False)
+    spacy_doc = sa.Column(sa.LargeBinary, nullable=True)
 
 
 class MessageNew(Base):
     __tablename__ = 'message_new'
 
-    id = sa.Column(sa.Integer(), nullable=False)
-    chat_id = sa.Column(sa.Integer(), nullable=False)
-    message_index = sa.Column(sa.Integer(), nullable=False)
-    summary = sa.Column(sa.String(), nullable=True)
-    message = sa.Column(sa.String(), nullable=False)
-    summary_tokens = sa.Column(sa.Integer(), nullable=True)
-    message_tokens = sa.Column(sa.Integer(), nullable=True)
-    created_date = sa.Column(sa.DateTime(), nullable=False)
-    spacy_doc = sa.Column(sa.LargeBinary(), nullable=True)
+    id = sa.Column(sa.Integer, primary_key=True)
+    chat_id = sa.Column(sa.Integer, nullable=False)
+    message_index = sa.Column(sa.Integer, nullable=False)
+    summary = sa.Column(sa.String, nullable=True)
+    message = sa.Column(sa.String, nullable=False)
+    summary_tokens = sa.Column(sa.Integer, nullable=True)
+    message_tokens = sa.Column(sa.Integer, nullable=True)
+    created_date = sa.Column(sa.DateTime, nullable=False)
+    spacy_doc = sa.Column(sa.LargeBinary, nullable=True)
 
 
 def upgrade() -> None:
@@ -124,9 +118,9 @@ def upgrade() -> None:
                     sa.ForeignKeyConstraint(['user_id'], ['user.id'], ),
                     sa.PrimaryKeyConstraint('id')
                     )
-    existing_chats = session.query(Message.chat_id).distinct()
+    existing_chats = session.query(MessageOld.chat_id).distinct()
     chats = {chat_id[0]: Chat(external_id=chat_id[0], user_id=default_user.id) for chat_id in existing_chats}
-    session.add_all(chats)
+    session.add_all(chats.values())
     session.commit()
 
     # Migrating the data and modifying Knowledge table
@@ -211,37 +205,82 @@ def downgrade() -> None:
     bind = op.get_bind()
     session = orm.Session(bind=bind)
 
-    chats = session.query(Chat).all()
+    chats = session.scalars(select(Chat))
+    chats = {chat.id: chat for chat in chats}
 
     # Moving old ids into Message table
-    op.drop_constraint(None, 'message', type_='foreignkey')
-    op.add_column('message', sa.Column('old_chat_id', sa.VARCHAR(), nullable=True))
-    for chat in chats:
-        bind.execute(text('UPDATE message SET old_chat_id = :external_id WHERE chat_id = :chat_id'),
-                     {
-                         'chat_id': chat.id,
-                         'external_id': chat.external_id
-                     })
-    op.drop_column('message', 'chat_id')
-    op.alter_column('message', 'new_chat_id',
-                    new_column_name='chat_id',
-                    existing_nullable=True,
-                    nullable=False)
+    op.rename_table('message', 'message_new')
+    op.create_table('message',
+                    sa.Column('id', sa.Integer(), nullable=False),
+                    sa.Column('chat_id', sa.String(), nullable=False),
+                    sa.Column('message_index', sa.Integer(), nullable=False),
+                    sa.Column('summary', sa.String(), nullable=True),
+                    sa.Column('message', sa.String(), nullable=False),
+                    sa.Column('summary_tokens', sa.Integer(), nullable=True),
+                    sa.Column('message_tokens', sa.Integer(), nullable=True),
+                    sa.Column('created_date', sa.DateTime(), nullable=False),
+                    sa.Column('spacy_doc', sa.LargeBinary(), nullable=True),
+                    sa.PrimaryKeyConstraint('id')
+                    )
+    query = select(MessageNew)
+    message_entries = session.scalars(query)
+    old_message_entries = []
+    for entry in message_entries:
+        new_entry = {
+            'id': entry.id,
+            'chat_id': chats[entry.chat_id].external_id,
+            'message_index': entry.message_index,
+            'summary': entry.summary,
+            'message': entry.message,
+            'summary_tokens': entry.summary_tokens,
+            'message_tokens': entry.message_tokens,
+            'created_date': entry.created_date,
+            'spacy_doc': entry.spacy_doc
+        }
+        old_message_entries.append(new_entry)
+
+    session.execute(insert(MessageOld), old_message_entries)
+    session.commit()
+    op.drop_table('message_new')
 
     # Moving old ids into Knowledge table
-    op.drop_constraint(None, 'knowledge', type_='foreignkey')
-    op.add_column('knowledge', sa.Column('old_chat_id', sa.VARCHAR(), nullable=True))
-    for chat in chats:
-        bind.execute(text('UPDATE knowledge SET old_chat_id = :external_id WHERE chat_id = :chat_id'),
-                     {
-                         'chat_id': chat.id,
-                         'external_id': chat.external_id
-                     })
-    op.drop_column('knowledge', 'chat_id')
-    op.alter_column('knowledge', 'new_chat_id',
-                    new_column_name='chat_id',
-                    existing_nullable=True,
-                    nullable=False)
+    op.rename_table('knowledge', 'knowledge_new')
+    op.create_table('knowledge',
+                    sa.Column('id', sa.Integer(), nullable=False),
+                    sa.Column('chat_id', sa.String(), nullable=False),
+                    sa.Column('entity', sa.String(), nullable=False),
+                    sa.Column('entity_type', sa.String(), nullable=True),
+                    sa.Column('entity_label', sa.String(), nullable=True),
+                    sa.Column('summary', sa.String(), nullable=True),
+                    sa.Column('token_count', sa.Integer(), nullable=True),
+                    sa.Column('updated_date', sa.DateTime(), nullable=False),
+                    sa.Column('update_at', sa.Integer(), nullable=True),
+                    sa.Column('update_count', sa.Integer(), nullable=True),
+                    sa.ForeignKeyConstraint(['chat_id'], ['chat.id'], ),
+                    sa.PrimaryKeyConstraint('id'))
+
+    query = select(KnowledgeNew)
+    knowledge_entries = session.scalars(query)
+    old_knowledge_entries = []
+
+    for entry in knowledge_entries:
+        new_entry = {
+            'id': entry.id,
+            'chat_id': chats[entry.chat_id].external_id,
+            'entity': entry.entity,
+            'entity_type': entry.entity_type,
+            'entity_label': entry.entity_label,
+            'summary': entry.summary,
+            'token_count': entry.token_count,
+            'updated_date': entry.updated_date,
+            'update_at': entry.update_at,
+            'update_count': entry.update_count,
+        }
+        old_knowledge_entries.append(new_entry)
+
+    session.execute(insert(KnowledgeOld), old_knowledge_entries)
+    session.commit()
+    op.drop_table('knowledge_new')
 
     # Dropping new tables
     op.drop_table('chat')
