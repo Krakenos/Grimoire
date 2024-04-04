7bb05639624f6b7d43bea9466acf85566d704084
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-04-04 12:26:49 +0200

    add postgres to Start.bat

--- Start.bat
+++ Start.bat
@@ -17,6 +17,7 @@ IF NOT EXIST "venv\" (
 CALL docker container inspect grimoire-redis
 IF ERRORLEVEL 1 (
     CALL docker run -d --name grimoire-redis -p 6379:6379 redis
+    CALL docker run --name grimoire-postgres -e POSTGRES_PASSWORD=secretpassword -e POSTGRES_USER=grimoire -d postgres
     IF ERRORLEVEL 1 (
         ECHO Error, docker is either not installed or not running.
         PAUSE
