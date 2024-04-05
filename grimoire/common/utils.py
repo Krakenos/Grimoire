ce312d64bf173bf1a94582bbd2d63c1c335f2de6
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-04-05 12:01:15 +0200

    small change in get_passthrough

--- grimoire/common/utils.py
+++ grimoire/common/utils.py
@@ -8,8 +8,8 @@ from grimoire.core.settings import settings
 def get_passthrough(endpoint: str, auth_token=None) -> dict:
 
     passthrough_url = urljoin(settings['main_api']['url'], endpoint)
-    kobold_response = requests.get(passthrough_url, headers={'Authorization': f'Bearer {auth_token}'})
-    return kobold_response.json()
+    engine = requests.get(passthrough_url, headers={'Authorization': f'Bearer {auth_token}'})
+    return engine.json()
 
 
 def orm_get_or_create(session, db_model, **kwargs):
