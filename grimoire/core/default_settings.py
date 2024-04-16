286b49a86d58beea68c5d21768795810c9a88836
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-04-16 13:17:22 +0200

    adding redis settings

--- grimoire/core/default_settings.py
+++ grimoire/core/default_settings.py
@@ -1,5 +1,7 @@
 defaults = {
     "CELERY_BROKER_URL": "redis://127.0.0.1:6379/0",
+    "REDIS_HOST": "localhost",
+    "REDIS_PORT": 6379,
     "DB_ENGINE": "postgresql+psycopg2://grimoire:secretpassword@127.0.0.1:5432/grimoire",
     "DEBUG": False,
     "LOG_PROMPTS": False,
