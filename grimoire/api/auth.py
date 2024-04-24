950a67f071168bede63a294b8612befbe3a93edc
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-04-24 16:46:31 +0200

    fix in auth

--- grimoire/api/auth.py
+++ grimoire/api/auth.py
@@ -7,7 +7,7 @@ from grimoire.core.settings import settings
 
 def check_api_key(authorization: Annotated[str | None, Header()]) -> None:
     grimoire_api_key = settings["AUTH_KEY"]
-    if grimoire_api_key is None or authorization == f"Bearer {grimoire_api_key}":
+    if not grimoire_api_key or authorization == f"Bearer {grimoire_api_key}":
         return
     else:
         raise HTTPException(status_code=401, detail="Unauthorized")
