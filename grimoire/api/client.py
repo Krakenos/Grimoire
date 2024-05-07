2faa0b2cfe23995ab6149d9d0e81df580db29a44
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-05-07 15:01:49 +0200

    add routes

--- grimoire/api/client.py
+++ grimoire/api/client.py
@@ -4,10 +4,11 @@ from fastapi.requests import Request
 from fastapi.responses import JSONResponse
 
 from grimoire.api.auth import check_api_key
-from grimoire.api.routers import kobold, oai_generic
+from grimoire.api.routers import grimoire, kobold, oai_generic
 from grimoire.common.loggers import general_logger
 
 app = FastAPI(dependencies=[Depends(check_api_key)])
+app.include_router(grimoire.router, prefix="/grimoire")
 app.include_router(kobold.router)
 app.include_router(oai_generic.router)
 
--- grimoire/api/routers/grimoire.py
+++ grimoire/api/routers/grimoire.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from grimoire.api import grimoire_utils, request_models
+from grimoire.db.connection import get_db
 
 router = APIRouter(tags=["Grimoire specific endpoints"])
 
@@ -8,9 +12,12 @@ def get_users():
     pass
 
 
-@router.get("/users/{user_id}")
-def get_user():
-    pass
+@router.get("/users/{user_id}", response_model=request_models.User)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = grimoire_utils.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
 
 @router.get("/users/{user_id}/chats")
