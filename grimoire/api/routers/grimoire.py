5ed3fdc9689e37b289614f3dd941d0acae76e3a3
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-05-07 14:17:49 +0200

    improve outline

--- grimoire/api/routers/grimoire.py
+++ grimoire/api/routers/grimoire.py
@@ -3,31 +3,56 @@ from fastapi import APIRouter
 router = APIRouter(tags=["Grimoire specific endpoints"])
 
 
-@router.get("/chats")
+@router.get("/users")
+def get_users():
+    pass
+
+
+@router.get("/users/{user_id}")
+def get_user():
+    pass
+
+
+@router.get("/users/{user_id}/chats")
 def get_chats():
     pass
 
 
-@router.put("/chats")
-def update_chats():
+@router.get("/users/{user_id}/chats/{chat_id}")
+def get_chat():
     pass
 
 
-@router.get("/messages")
+@router.put("/users/{user_id}/chats/{chat_id}")
+def update_chat():
+    pass
+
+
+@router.get("/users/{user_id}/chats/{chat_id}/messages")
 def get_messages():
     pass
 
 
-@router.put("/messages")
-def update_messages():
+@router.get("/users/{user_id}/chats/{chat_id}/messages/{message_id}")
+def get_message():
     pass
 
 
-@router.get("/knowledge")
+@router.put("/users/{user_id}/chats/{chat_id}/messages/{message_id}")
+def update_message():
+    pass
+
+
+@router.get("/users/{user_id}/chats/{chat_id}/knowledge")
+def get_all_knowledge():
+    pass
+
+
+@router.get("/users/{user_id}/chats/{chat_id}/knowledge/{knowledge_id}")
 def get_knowledge():
     pass
 
 
-@router.put("/knowledge")
+@router.put("/users/{user_id}/chats/{chat_id}/knowledge/{knowledge_id}")
 def update_knowledge():
     pass
