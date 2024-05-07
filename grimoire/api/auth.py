607f78e74c6d7e2a30f0228466c7cf5efdf8c827
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-05-07 15:01:29 +0200

    fix for non optional header

--- grimoire/api/auth.py
+++ grimoire/api/auth.py
@@ -5,7 +5,7 @@ from fastapi import Header, HTTPException
 from grimoire.core.settings import settings
 
 
-def check_api_key(authorization: Annotated[str | None, Header()]) -> None:
+def check_api_key(authorization: Annotated[str | None, Header()] = None) -> None:
     grimoire_api_key = settings["AUTH_KEY"]
     if not grimoire_api_key or authorization == f"Bearer {grimoire_api_key}":
         return
