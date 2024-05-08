e8c22b3a13273566af203c67a3fb48ca2387def4
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-05-08 14:09:04 +0200

    chat endpoints

--- grimoire/api/routers/grimoire.py
+++ grimoire/api/routers/grimoire.py
@@ -21,14 +21,18 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@router.get("/users/{user_id}/chats")
-def get_chats():
-    pass
+@router.get("/users/{user_id}/chats", response_model=list[request_models.Chat])
+def get_chats(user_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    chats = grimoire_utils.get_chats(db, user_id=user_id, skip=skip, limit=limit)
+    return chats
 
 
-@router.get("/users/{user_id}/chats/{chat_id}")
-def get_chat():
-    pass
+@router.get("/users/{user_id}/chats/{chat_id}", response_model=request_models.Chat)
+def get_chat(user_id: int, chat_id: int, db: Session = Depends(get_db)):
+    db_chat = grimoire_utils.get_chat(db, user_id=user_id, chat_id=chat_id)
+    if db_chat is None:
+        raise HTTPException(status_code=404, detail="Chat not found")
+    return db_chat
 
 
 @router.put("/users/{user_id}/chats/{chat_id}")
