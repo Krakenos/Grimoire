7098c4ecbbf72da77fa3e1f0ff83373f1708add3
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-05-08 13:29:02 +0200

    queries for chat endpoints

--- grimoire/api/grimoire_utils.py
+++ grimoire/api/grimoire_utils.py
@@ -1,10 +1,12 @@
+from collections.abc import Sequence
+
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from grimoire.db.models import User
+from grimoire.db.models import Chat, User
 
 
-def get_users(db_session: Session, skip: int = 0, limit: int = 100):
+def get_users(db_session: Session, skip: int = 0, limit: int = 100) -> Sequence[User]:
     query = select(User).offset(skip).limit(limit)
     results = db_session.scalars(query).all()
     return results
@@ -12,5 +14,17 @@ def get_users(db_session: Session, skip: int = 0, limit: int = 100):
 
 def get_user(db_session: Session, user_id: int) -> User:
     query = select(User).where(User.id == user_id)
+    results = db_session.scalar(query)
+    return results
+
+
+def get_chats(db_session: Session, user_id: int, skip: int = 0, limit: int = 100) -> Sequence[Chat]:
+    query = select(Chat).where(Chat.user_id == user_id).offset(skip).limit(limit)
+    results = db_session.scalars(query).all()
+    return results
+
+
+def get_chat(db_session: Session, user_id: int, chat_id: int) -> Chat:
+    query = select(Chat).where(Chat.user_id == user_id, Chat.id == chat_id)
     results = db_session.scalar(query)
     return results
