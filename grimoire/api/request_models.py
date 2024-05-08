691c5b27d4f498a7d0f4e9313675e8273ca3b0b7
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-05-08 12:51:31 +0200

    adding basic request models for api

--- grimoire/api/request_models.py
+++ grimoire/api/request_models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pydantic import BaseModel, ConfigDict, PositiveInt
 
 
@@ -87,3 +89,22 @@ class OAITokenEncode(BaseModel):
 class User(BaseModel):
     id: int
     external_id: str
+
+
+class Chat(BaseModel):
+    id: int
+    external_id: str
+
+
+class ChatMessage(BaseModel):
+    id: int
+    message_index: int
+    message: int
+    created_date: datetime
+
+
+class Knowledge(BaseModel):
+    id: int
+    entity: str
+    summary: str
+    updated_date: datetime
