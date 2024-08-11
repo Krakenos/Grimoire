06bd2fc298e7163a8991d90ee41160846c4b1480
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-08-11 22:50:20 +0200

    fuzzy search in queries

--- grimoire/db/queries.py
+++ grimoire/db/queries.py
@@ -1,21 +1,42 @@
+from rapidfuzz import fuzz, process, utils
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
+from grimoire.core.settings import settings
 from grimoire.db.models import Knowledge
 
 
 def get_knowledge_entity(term: str, chat_id: int, session: Session) -> Knowledge | None:
     query = select(Knowledge).where(Knowledge.entity_type == "NAMED ENTITY", Knowledge.chat_id == chat_id)
     query_results = session.scalars(query).all()
-    knowledge_dict = {knowledge_obj.entity.lower(): knowledge_obj for knowledge_obj in query_results}
-    if term.lower() in knowledge_dict:
-        return knowledge_dict[term.lower()]
-    return None
+    knowledge_dict = {knowledge_obj.entity: knowledge_obj for knowledge_obj in query_results}
+    found_entry = process.extractOne(
+        term,
+        list(knowledge_dict),
+        scorer=fuzz.WRatio,
+        processor=utils.default_process,
+        score_cutoff=settings["match_distance"],
+    )
+    if found_entry is None:
+        return None
+    ent_name, _, _ = found_entry
+    return knowledge_dict[ent_name]
 
 
 def get_knowledge_entities(terms: list[str], chat_id: int, session: Session) -> list[Knowledge]:
     query = select(Knowledge).where(Knowledge.entity_type == "NAMED ENTITY", Knowledge.chat_id == chat_id)
     query_results = session.scalars(query).all()
-    knowledge_dict = {knowledge_obj.entity.lower(): knowledge_obj for knowledge_obj in query_results}
-    results = [knowledge_dict[term.lower()] for term in terms if term.lower() in knowledge_dict]
+    results = []
+    knowledge_dict = {knowledge_obj.entity: knowledge_obj for knowledge_obj in query_results}
+    for term in terms:
+        found_entry = process.extractOne(
+            term,
+            list(knowledge_dict),
+            scorer=fuzz.WRatio,
+            processor=utils.default_process,
+            score_cutoff=settings["match_distance"],
+        )
+        if found_entry is not None:
+            ent_name, _, _ = found_entry
+            results.append(knowledge_dict[ent_name])
     return results
