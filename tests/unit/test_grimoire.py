1907670a771a5588eefa17ca488599a611c30ec6
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-08-15 16:33:10 +0200

    add tests

--- tests/unit/test_grimoire.py
+++ tests/unit/test_grimoire.py
@@ -10,3 +10,21 @@ class TestFilterSimilarEntities(TestCase):
         self.assertEqual(test_results["John"], "John")
         self.assertEqual(test_results["Alex"], "Alex")
         self.assertEqual(test_results["Jack"], "Jack")
+
+    def test_filter_with_relations(self):
+        test_data = ["Alex", "Alexy", "Alexey"]
+        test_results = filter_similar_entities(test_data)
+        self.assertEqual(test_results["Alex"], "Alexey")
+        self.assertEqual(test_results["Alexy"], "Alexey")
+        self.assertEqual(test_results["Alexey"], "Alexey")
+
+    def test_filter_mixed(self):
+        test_data = ["Alex", "Alexy", "Alexey", "John", "Jack", "Jerem", "Jeremy"]
+        test_results = filter_similar_entities(test_data)
+        self.assertEqual(test_results["Alex"], "Alexey")
+        self.assertEqual(test_results["Alexy"], "Alexey")
+        self.assertEqual(test_results["Alexey"], "Alexey")
+        self.assertEqual(test_results["John"], "John")
+        self.assertEqual(test_results["Jack"], "Jack")
+        self.assertEqual(test_results["Jerem"], "Jeremy")
+        self.assertEqual(test_results["Jeremy"], "Jeremy")
