95da2b05808a178fcba90f23c004981ea686bebc
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-08-16 15:13:10 +0200

    remove context logger

--- grimoire/common/loggers.py
+++ grimoire/common/loggers.py
@@ -17,29 +17,22 @@ formatter = logging.Formatter("[%(asctime)s] %(levelname)s [%(module)s:%(funcNam
 
 general_logger = logging.getLogger("general")
 summary_logger = logging.getLogger("summary")
-context_logger = logging.getLogger("context")
 
 general_logger.setLevel(LOG_LEVEL)
 summary_logger.setLevel(PROMPT_LEVEL)
-context_logger.setLevel(PROMPT_LEVEL)
 
 if settings["LOG_FILES"]:
     GENERAL_LOG_FILE = "general.log"
     SUMMARY_LOG_FILE = "summary.log"
-    CONTEXT_LOG_FILE = "context.log"
 
     general_file_handler = logging.FileHandler(GENERAL_LOG_FILE)
     summary_file_handler = logging.FileHandler(SUMMARY_LOG_FILE)
-    context_file_handler = logging.FileHandler(CONTEXT_LOG_FILE)
 
     general_file_handler.setFormatter(formatter)
     summary_file_handler.setFormatter(formatter)
-    context_file_handler.setFormatter(formatter)
 
     general_logger.addHandler(general_file_handler)
     summary_logger.addHandler(summary_file_handler)
-    context_logger.addHandler(context_file_handler)
 
 general_logger.addHandler(logging.StreamHandler())
 summary_logger.addHandler(logging.StreamHandler())
-context_logger.addHandler(logging.StreamHandler())
