db7f7319e2964d0de6a010bbdeb227124715d10e
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-08-16 14:03:23 +0200

    remove routes

--- grimoire/api/client.py
+++ grimoire/api/client.py
@@ -4,13 +4,11 @@ from fastapi.requests import Request
 from fastapi.responses import JSONResponse
 
 from grimoire.api.auth import check_api_key
-from grimoire.api.routers import grimoire, kobold, oai_generic
+from grimoire.api.routers import grimoire
 from grimoire.common.loggers import general_logger
 
 app = FastAPI(dependencies=[Depends(check_api_key)])
 app.include_router(grimoire.router, prefix="/grimoire")
-app.include_router(kobold.router)
-app.include_router(oai_generic.router)
 
 
 @app.exception_handler(RequestValidationError)
