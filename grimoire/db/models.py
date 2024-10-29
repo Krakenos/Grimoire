a9a0a29775d5a528c42791be234bb3271de191c3
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-10-29 15:22:47 +0100

    replace sender name with character id

--- grimoire/db/models.py
+++ grimoire/db/models.py
@@ -55,7 +55,8 @@ class Message(Base):
     chat_id: Mapped[int] = mapped_column(ForeignKey("chat.id"))
     chat: Mapped["Chat"] = relationship(back_populates="messages")
     message_index: Mapped[int]
-    sender_name = Column(StringEncryptedType(Unicode, encryption_key, AesEngine, "pkcs5"), nullable=True)
+    character_id: Mapped[int] = mapped_column(ForeignKey("character.id"))
+    character: Mapped["Character"] = relationship()
     message = Column(StringEncryptedType(Unicode, encryption_key, AesEngine, "pkcs5"), nullable=True)
     created_date: Mapped[datetime] = mapped_column(default=datetime.now)
     spacy_named_entities: Mapped[list["SpacyNamedEntity"]] = relationship()
